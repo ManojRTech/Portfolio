3bab221e6d239bf69ad687dfafdcbe3803ddcc5f
Manoj.R <161286205+ManojRTech@users.noreply.github.com>
2025-09-02 19:06:47 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import { ArrowDown } from "lucide-react";
 export const HeroSection = () => {
     return (
         <section id="hero" className="relative min-h-screen flex fles-col items-center justify-center px-4">
-            <div className="container max-w-4x; mx-auto text-center z-10">
+            <div className="container max-w-4x; mx-auto text-center z-10 text-center max-w-3xl mx-auto">
                 <div className="space-y-6">
                     <h1 className="text-4xl md:text-4xl font-bold tracking-tight">
                         <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
