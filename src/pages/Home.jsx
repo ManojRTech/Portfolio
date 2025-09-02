8b2de1dba095cd5a62b9d2051bd4cb1c13b14fb0
Manoj.R <161286205+ManojRTech@users.noreply.github.com>
2025-09-02 19:00:26 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,9 +12,7 @@ export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
             {/* Theme Toggle */}
-            <div className="flex justify-end items-center">
-              <ThemeToggle />
-            </div>
+            <ThemeToggle />
 
 
             {/* Background Effects */}
